a278636d39e0b573ea5fbf48a6ca3bbcf9d2cb19
muhammedrishadpe <rishadpe@gmail.com>
2021-06-05 12:49:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let Counter = 1;
+const size = carouselImages[0].clientWidth;
