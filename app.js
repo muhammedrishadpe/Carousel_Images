125fdebe7be6524c90f59bd71844c835d264be30
muhammedrishadpe <rishadpe@gmail.com>
2021-06-07 12:41:10 +0530

    updated

--- app.js
+++ app.js
@@ -6,5 +6,38 @@ const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
 //Counter
-let Counter = 1;
+let counter = 1;
 const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//button Listeners
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImages.length -1) return;
+    carouselSlide.style.transition = "transform 1s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+   if (carouselImages[counter].id === 'lastClone'){
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length -2 ;
+    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
+
+   }
+   if (carouselImages[counter].id === 'lastClone'){
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter ;
+    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
+
+   }
+});
